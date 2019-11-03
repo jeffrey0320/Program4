8b3d344b33f7f0b4b4dcab46f6aa191c05d5b93f
jeffrey lopez <jeffreysofly0320@gmail.com>
2019-11-03 13:36:29 -0500

    Added methods

--- src/Program4.java
+++ src/Program4.java
@@ -1,5 +1,70 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Program4 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        Bank bankObj = new Bank();
+
+        char choice;
+        boolean notDone = true;
+
+        Scanner kybd = new Scanner(System.in);
+
+        PrintWriter outputFile = new PrintWriter("myoutput.txt");
+
+    }
+
+    public static void readAccts(){
+    
+    }
+
+    public static void printAccts(){
+
+    }
+
+    public static void menu(){
+
+    }
+
+    public static void balance(){
+
+    }
+
+    public static void deposit(){
+
+    }
+
+    public static void withdrawal(){
+
+    }
+
+    public static void clearCheck(){
+
+    }
+
+    public static void acctInfo(){
+
+    }
+
+    public static void acctInfoHistory(){
+
+    }
+
+    public static void newAccount(){
+
+    }
+
+    public static void closeAccount(){
+
+    }
+
+    public static void reopenAccount(){
+
+    }
+
+    public static void deleteAccount(){
 
     }
 }
